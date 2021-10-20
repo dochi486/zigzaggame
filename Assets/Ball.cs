7c8156e2a6ffbf3e6233bba79a4634e1f8e35dcf
dochi486 <93mskim@gmail.com>
2021-10-20 19:34:54 +0900

    공 움직이는 코드 작성

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    //방향을 쉽게 이해하기 위해 enum으로 관리
+    public enum Direction
     {
-        
+        Right,
+        Left,
     }
 
+    Direction direction = Direction.Right;
+
+
+    public float speed = 5;
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.anyKeyDown)
+        {
+            direction = (direction == Direction.Right) ? Direction.Left : Direction.Right;
+        }
+
+        BallMove();
+    }
+
+    private void BallMove()
+    {
+        //Vector3 move = Vector3.zero;
+
+        Vector3 move = (direction == Direction.Right) ? new Vector3(1, 0, 0) : new Vector3(0, 0, 1);
+
+        //if (direction == Direction.Right)
+        //    move = new Vector3(1, 0, 0);
+        //else
+        //    move = new Vector3(0, 0, 1);
+
+        transform.Translate(move * speed * Time.deltaTime, Space.Self);//self가 로컬축
     }
 }
